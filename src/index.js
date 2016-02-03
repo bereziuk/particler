cc26d8b96fecc017472606932a3919a3f083e9b5
Adam Bereziuk <adambereziuk@wp.pl>
2016-02-03 21:19:10 +0100

    ts migartion

--- src/index.js
+++ src/index.js
@@ -1,152 +1,128 @@
-/**
- * Created by Adam on 01.01.2015.sadad
- */
-
-Particles = function (settings) {
-    'use strict';
-
-    //window.onresize = function () {
-    //    init();
-    //};
-
-    var config = {
-            wrapperId: settings.id,
-            dots: {
-                length: settings.dotsLength || 20,
-                lineWidth: settings.lineWidth || 0.02,
-                fillColor: settings.fillColor || '#000000',
-                minSize: 0.5,
-                maxSize: 1.5,
-                minLineLength: 250,
-                speed: 25,
-                framesPerSecond: 60,
-                frameDuration: function () {
-                    return 1000 / this.framesPerSecond
-                }
-            }
-        },
-        dotsArray = [],
-        wrapper = document.getElementById(config.wrapperId),
-        dot = wrapper.getContext("2d"),
-        initDot = function (i, arr) {
-            var size,
-                vx,
-                vy,
-                posX,
-                posY,
-                angle = parseInt(Math.random() * 360, 10),
-                rads = angle * Math.PI / 180;
-
-            // set size and position
-            size = Math.floor(Math.random() * (config.dots.maxSize - config.dots.minSize + 1 ) + config.dots.minSize);
-            posX = parseInt(Math.random() * wrapper.offsetWidth, 10);
-            posY = parseInt(Math.random() * wrapper.offsetHeight, 10);
-
-            //vx = Math.cos(rads) * config.dots.speed / config.dots.framesPerSecond;
-            vx = Math.cos(rads) * parseFloat(config.dots.speed / config.dots.frameDuration());
-            vy = Math.sin(rads) * parseFloat(config.dots.speed / config.dots.frameDuration());
-
-            //set dots coordinates
-            arr[i] = {
-                size: size,
-                posX: posX,
-                posY: posY,
-                vx: vx,
-                vy: vy
-            };
-        },
-        drawDots = function () {
-            var i,
-                j = config.dots.length,
-                k,
-                el,
-                getDistance = function (x1, y1, x2, y2) {
-                    return parseInt(Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1-y2), 2)), 10);
-                };
-
-
-            for (i = 0; i < j; i++) {
-                // define dots positions
-                el = dotsArray[i];
-                el.posX += el.vx;
-                if (el.posX < 0 || el.posX > wrapper.offsetWidth) {
-                    el.vx = 0 - el.vx;
-                }
-
-                el.posY += el.vy;
-                if (el.posY < 0 || el.posY > wrapper.offsetHeight) {
-                    el.vy = 0 - el.vy;
-                }
-
-                // draw dots
-                dot.beginPath();
-                dot.fillStyle = config.dots.fillColor;
-                dot.arc(el.posX, el.posY, el.size, 0, 2 * Math.PI);
-
-                // draw lines between dots
-                for (k = 0; k < j; k++) {
-                    if (k !== i) {
-
-                        dot.lineWidth = config.dots.lineWidth;
-                        dot.strokeStyle = config.dots.fillColor;
-                        dot.moveTo(el.posX, el.posY);
-
-                        if (getDistance(el.posX, el.posY, dotsArray[k].posX, dotsArray[k].posY) < config.dots.minLineLength) {
-                            dot.lineTo(dotsArray[k].posX, dotsArray[k].posY);
-                            dot.stroke();
-                        }
+var ParticlerDefaultConfig = (function () {
+    function ParticlerDefaultConfig() {
+        this.quantity = 20;
+        this.lineWidth = 0.05;
+        this.fillColor = "#000000";
+        this.minSize = 1;
+        this.maxSize = 3;
+        this.minimalLineLength = 250;
+        this.speed = 25;
+        this.framesPerSecond = 60;
+        this.frameDuration = 20;
+    }
+    return ParticlerDefaultConfig;
+})();
+var Particler = (function () {
+    function Particler(wrapperId, customConfig) {
+        this.wrapperId = wrapperId;
+        this.config = new ParticlerDefaultConfig();
+        this.dotsArray = [];
+        this.wrapper = document.getElementById(wrapperId);
+        this.canvas = this.wrapper.getContext("2d");
+        this.setConfig(customConfig);
+        this.setWrapperSize();
+        this.generateDotsArray();
+        this.resizingHandler();
+    }
+    Particler.prototype.setConfig = function (customConfig) {
+        var _this = this;
+        if (customConfig !== undefined) {
+            (function () {
+                var item;
+                for (item in _this.config) {
+                    if (customConfig.hasOwnProperty(item) && _this.config[item] !== undefined) {
+                        _this.config[item] = customConfig[item];
                     }
                 }
-                dot.fill();
-            }
-
-            if (config.dots.fillAlpha) {
-                dot.fillStyle = config.dots.fillColor;
-            } else {
-                dot.fillStyle = config.dots.fillColor;
-            }
-
-            setTimeout(function () {
-                dot.clearRect(0, 0, wrapper.width, wrapper.height);
-                drawDots();
-            }, config.dots.frameDuration());
-        },
-        generateDotsArray = function () {
-            var i = 0;
-            for (i; i < config.dots.length; i++) {
-                initDot(i, dotsArray);
-            }
-            drawDots();
-        },
-        setWrapperSize = function (isResized) {
-            dot.canvas.width = wrapper.offsetWidth;
-            dot.canvas.height = wrapper.offsetHeight;
-            if (isResized) {
-                // TODO - redraw canvas
-            }
-        },
-        init = function () {
-            setWrapperSize();
-            generateDotsArray();
+            })();
+        }
+    };
+    ;
+    Particler.prototype.createDot = function (i, arr) {
+        var size;
+        var vx;
+        var vy;
+        var posX;
+        var posY;
+        var angle = Math.random() * 360;
+        var rads = angle * Math.PI / 180;
+        // setting size and position
+        size = Math.floor(Math.random() * (this.config.maxSize - this.config.minSize + 1) + this.config.minSize);
+        posX = Math.random() * this.wrapper.offsetWidth;
+        posY = Math.random() * this.wrapper.offsetHeight;
+        vx = Math.cos(rads) * (this.config.speed / this.config.frameDuration);
+        vy = Math.sin(rads) * (this.config.speed / this.config.frameDuration);
+        // setting dot coordinates
+        arr[i] = {
+            size: size,
+            posX: posX,
+            posY: posY,
+            vx: vx,
+            vy: vy
         };
-
-    //window.onresize = function () {
-    //    setWrapperSize(true);
-    //};
-
-    init();
-
-};
-
-var firstExample = new Particles({
-        id: 'particles_1',
-        lineWidth: 0.25,
-        fillColor: '#46ecd4',
-        dotsLength: 25
-    }),
-    secondExample = new Particles({
-        id: 'particles_2',
-        fillColor: '#fff',
-        lineWidth: 0.5,
-        dotsLength: 25
-    });
+    };
+    Particler.prototype.drawDots = function () {
+        var _this = this;
+        var i, j = this.config.quantity, k, el, getDistance = function (x1, y1, x2, y2) {
+            return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+        };
+        for (i = 0; i < j; i++) {
+            // define dots positions
+            el = this.dotsArray[i];
+            el.posX += el.vx;
+            if (el.posX < 0 || el.posX > this.wrapper.offsetWidth) {
+                if (el.posX < 0) {
+                    el.posX = 0;
+                }
+                else {
+                    el.posX = this.wrapper.offsetWidth;
+                }
+                el.vx = -el.vx;
+            }
+            el.posY += el.vy;
+            if (el.posY < 0 || el.posY > this.wrapper.offsetHeight) {
+                el.vy = -el.vy;
+            }
+            // draw dots
+            this.canvas.beginPath();
+            this.canvas.fillStyle = this.config.fillColor;
+            this.canvas.arc(el.posX, el.posY, el.size, 0, 2 * Math.PI);
+            // draw lines between dots
+            for (k = 0; k < j; k++) {
+                if (k !== i) {
+                    this.canvas.lineWidth = this.config.lineWidth;
+                    this.canvas.strokeStyle = this.config.fillColor;
+                    this.canvas.moveTo(el.posX, el.posY);
+                    if (getDistance(el.posX, el.posY, this.dotsArray[k].posX, this.dotsArray[k].posY) < this.config.minimalLineLength) {
+                        this.canvas.lineTo(this.dotsArray[k].posX, this.dotsArray[k].posY);
+                        this.canvas.stroke();
+                    }
+                }
+            }
+            this.canvas.fill();
+        }
+        this.canvas.fillStyle = this.config.fillColor;
+        setTimeout(function () {
+            _this.canvas.clearRect(0, 0, _this.wrapper.width, _this.wrapper.height);
+            _this.drawDots();
+        }, this.config.frameDuration);
+    };
+    Particler.prototype.generateDotsArray = function () {
+        var i = 0;
+        for (i; i < this.config.quantity; i++) {
+            this.createDot(i, this.dotsArray);
+        }
+        this.drawDots();
+    };
+    Particler.prototype.setWrapperSize = function () {
+        this.canvas.canvas.width = this.wrapper.offsetWidth;
+        this.canvas.canvas.height = this.wrapper.offsetHeight;
+    };
+    Particler.prototype.resizingHandler = function () {
+        var _this = this;
+        window.addEventListener('resize', function () {
+            _this.setWrapperSize();
+        });
+    };
+    return Particler;
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-interface IDotsConfig {
+interface IParticlerConfig {
+    quantity: number;
+    lineWidth: number;
+    fillColor: string;
+    minSize: number;
+    maxSize: number;
+    minimalLineLength: number;
+    speed: number;
+    framesPerSecond: number;
+    frameDuration: number;
+}
+
+interface IParticlerCustomConfig {
     quantity?: number;
     lineWidth?: number;
     fillColor?: string;
@@ -10,170 +22,167 @@ interface IDotsConfig {
     frameDuration?: number;
 }
 
-class MoleculesConfig {
-    constructor() {
-        var quantity = 20;
-        var lineWidth = 0.05;
-        var fillColor = "#000000";
-        var minSize = 1;
-        var maxSize = 3;
-        var minimalLineLength = 250;
-        var speed = 25;
-        var framesPerSecond = 60;
-        var frameDuration = 20;
+interface iDot {
+    size: number;
+    posX: number;
+    posY: number;
+    vx: number;
+    vy: number;
+}
+
+interface IParticler {
+    wrapperId: string;
+    wrapper: HTMLCanvasElement;
+    canvas: CanvasRenderingContext2D;
+    config: IParticlerConfig;
+    dotsArray: Array<iDot>;
+}
+
+class ParticlerDefaultConfig implements IParticlerConfig {
+    quantity = 20;
+    lineWidth = 0.05;
+    fillColor = "#000000";
+    minSize = 1;
+    maxSize = 3;
+    minimalLineLength = 250;
+    speed = 25;
+    framesPerSecond = 60;
+    frameDuration = 20;
+}
+
+class Particler implements IParticler {
+    wrapper: HTMLCanvasElement;
+    canvas: CanvasRenderingContext2D;
+    config: IParticlerConfig = new ParticlerDefaultConfig();
+    dotsArray: Array<iDot> = [];
+
+    constructor(public wrapperId: string, customConfig?: IParticlerCustomConfig) {
+        this.wrapper = <HTMLCanvasElement> document.getElementById(wrapperId);
+        this.canvas = <CanvasRenderingContext2D> this.wrapper.getContext("2d");
+        this.setConfig(customConfig);
+        this.setWrapperSize();
+        this.generateDotsArray();
+        this.resizingHandler();
     }
-}
-
-interface IMolecules {
-    wrapperId: string;
-    config?: IDotsConfig;
-    wrapper: any;
-    canvas: any;
-
-}
-
-class Molecules implements IMolecules {
-    wrapperId: string;
-    dotsArray = [];
-    defaultConfig = new MoleculesConfig();
-    config;
-    wrapper;
-    canvas;
-    createDot;
-    drawDots;
-    generateDotsArray;
-
-    constructor(id: string, settings: any) {
-
-        this.wrapperId = id;
-        this.wrapper = document.getElementById(this.wrapperId);
-        this.canvas = this.wrapper.getContext("2d");
-
-        if (settings !== undefined) {
+    
+    setConfig(customConfig: IParticlerCustomConfig): void {
+        if (customConfig !== undefined) {
             (() => {
                 var item: any;
 
-                for (item in settings) {
-                    if (settings.hasOwnProperty(item) && this.config.dots[item] !== undefined) {
-                        this.config.dots[item] = settings[item];
+                for (item in this.config) {
+                    if (customConfig.hasOwnProperty(item) && this.config[item] !== undefined) {
+                        this.config[item] = customConfig[item];
                     }
                 }
             })();
         }
+    };
 
-        createDot(i: any, arr: any): void {
-            var size,
-                vx,
-                vy,
-                posX,
-                posY,
-                angle = Math.random() * 360,
-                rads = angle * Math.PI / 180;
+    createDot(i: any, arr: any): void {
+        let size;
+        let vx;
+        let vy;
+        let posX;
+        let  posY;
+        let  angle = Math.random() * 360;
+        let  rads = angle * Math.PI / 180;
 
-            // setting size and position
-            size = Math.floor(Math.random() * (this.config.dots.maxSize - this.config.dots.minSize + 1) + this.config.dots.minSize);
-            posX = Math.random() * this.wrapper.offsetWidth;
-            posY = Math.random() * this.wrapper.offsetHeight;
+        // setting size and position
+        size = Math.floor(Math.random() * (this.config.maxSize - this.config.minSize + 1) + this.config.minSize);
+        posX = Math.random() * this.wrapper.offsetWidth;
+        posY = Math.random() * this.wrapper.offsetHeight;
 
-            vx = Math.cos(rads) * (this.config.dots.speed / this.config.dots.frameDuration);
-            vy = Math.sin(rads) * (this.config.dots.speed / this.config.dots.frameDuration);
+        vx = Math.cos(rads) * (this.config.speed / this.config.frameDuration);
+        vy = Math.sin(rads) * (this.config.speed / this.config.frameDuration);
 
-            // setting dot coordinates
-            arr[i] = {
-                size: size,
-                posX: posX,
-                posY: posY,
-                vx: vx,
-                vy: vy
-            };
-        }
-
-        drawDots() {
-            var i,
-                j = this.config.dots.quantity,
-                k,
-                el,
-                getDistance = (x1: number, y1: number, x2: number, y2: number) => {
-                    return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-                };
-
-            for (i = 0; i < j; i++) {
-                // define dots positions
-                el = this.dotsArray[i];
-                el.posX += el.vx;
-
-                if (el.posX < 0 || el.posX > this.wrapper.offsetWidth) {
-                    if (el.posX < 0) {
-                        el.posX = 0;
-                    } else {
-                        el.posX = this.wrapper.offsetWidth;
-                    }
-
-                    el.vx = -el.vx;
-                }
-
-                el.posY += el.vy;
-                if (el.posY < 0 || el.posY > this.wrapper.offsetHeight) {
-                    el.vy = -el.vy;
-                }
-
-                // draw dots
-                this.canvas.beginPath();
-                this.canvas.fillStyle = this.config.dots.fillColor;
-                this.canvas.arc(el.posX, el.posY, el.size, 0, 2 * Math.PI);
-
-                // draw lines between dots
-                for (k = 0; k < j; k++) {
-                    if (k !== i) {
-
-                        this.canvas.lineWidth = this.config.dots.lineWidth;
-                        this.canvas.strokeStyle = this.config.dots.fillColor;
-                        this.canvas.moveTo(el.posX, el.posY);
-
-                        if (getDistance(el.posX, el.posY, this.dotsArray[k].posX, this.dotsArray[k].posY) < this.config.dots.minimalLineLength) {
-                            this.canvas.lineTo(this.dotsArray[k].posX, this.dotsArray[k].posY);
-                            this.canvas.stroke();
-                        }
-                    }
-                }
-                this.canvas.fill();
-            }
-
-            this.canvas.fillStyle = this.config.dots.fillColor;
-
-            setTimeout(() => {
-                this.canvas.clearRect(0, 0, this.wrapper.width, this.wrapper.height);
-                this.drawDots();
-            }, this.config.dots.frameDuration);
-        }
-
-        generateDotsArray:() => {
-            var i = 0;
-
-            for (i; i < this.config.dots.quantity; i++) {
-                this.createDot(i, this.dotsArray);
-            }
-
-            drawDots();
-        }
-
-        setWrapperSize() {
-            this.canvas.canvas.width = this.wrapper.offsetWidth;
-            this.canvas.canvas.height = this.wrapper.offsetHeight;
-        }
-
-        resizingHandler(): void {
-            window.addEventListener('resize',() => {
-                setWrapperSize();
-            });
-        }
-
-        init(): void {
-            setWrapperSize();
-            generateDotsArray();
-            resizingHandler();
+        // setting dot coordinates
+        arr[i] = {
+            size: size,
+            posX: posX,
+            posY: posY,
+            vx: vx,
+            vy: vy
         };
+    }
 
-        init();
+    drawDots(): void {
+        var i,
+            j = this.config.quantity,
+            k,
+            el,
+            getDistance = (x1: number, y1: number, x2: number, y2: number) => {
+                return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+            };
+
+        for (i = 0; i < j; i++) {
+            // define dots positions
+            el = this.dotsArray[i];
+            el.posX += el.vx;
+
+            if (el.posX < 0 || el.posX > this.wrapper.offsetWidth) {
+                if (el.posX < 0) {
+                    el.posX = 0;
+                } else {
+                    el.posX = this.wrapper.offsetWidth;
+                }
+
+                el.vx = -el.vx;
+            }
+
+            el.posY += el.vy;
+            if (el.posY < 0 || el.posY > this.wrapper.offsetHeight) {
+                el.vy = -el.vy;
+            }
+
+            // draw dots
+            this.canvas.beginPath();
+            this.canvas.fillStyle = this.config.fillColor;
+            this.canvas.arc(el.posX, el.posY, el.size, 0, 2 * Math.PI);
+
+            // draw lines between dots
+            for (k = 0; k < j; k++) {
+                if (k !== i) {
+
+                    this.canvas.lineWidth = this.config.lineWidth;
+                    this.canvas.strokeStyle = this.config.fillColor;
+                    this.canvas.moveTo(el.posX, el.posY);
+
+                    if (getDistance(el.posX, el.posY, this.dotsArray[k].posX, this.dotsArray[k].posY) < this.config.minimalLineLength) {
+                        this.canvas.lineTo(this.dotsArray[k].posX, this.dotsArray[k].posY);
+                        this.canvas.stroke();
+                    }
+                }
+            }
+            this.canvas.fill();
+        }
+
+        this.canvas.fillStyle = this.config.fillColor;
+
+        setTimeout(() => {
+            this.canvas.clearRect(0, 0, this.wrapper.width, this.wrapper.height);
+            this.drawDots();
+        }, this.config.frameDuration);
+    }
+
+    generateDotsArray(): void {
+        var i = 0;
+
+        for (i; i < this.config.quantity; i++) {
+            this.createDot(i, this.dotsArray);
+        }
+
+        this.drawDots();
+    }
+
+    setWrapperSize() {
+        this.canvas.canvas.width = this.wrapper.offsetWidth;
+        this.canvas.canvas.height = this.wrapper.offsetHeight;
+    }
+
+    resizingHandler(): void {
+        window.addEventListener('resize',() => {
+            this.setWrapperSize();
+        });
     }
 }
